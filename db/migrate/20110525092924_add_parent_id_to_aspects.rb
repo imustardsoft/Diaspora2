492aa1552f6a86d0077429929b332a9d9a601c04
liyunqiang <liyunqiang@www.give.bz>
2011-05-31 15:41:17 +0800

    delete migration parent_id

--- db/migrate/20110525092924_add_parent_id_to_aspects.rb
+++
@@ -1,9 +0,0 @@
-class AddParentIdToAspects < ActiveRecord::Migration
-  def self.up
-    add_column :aspects, :parent_id, :integer
-  end
-
-  def self.down
-    remove_column :aspects, :parent_id
-  end
-end
